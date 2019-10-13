6f0e840069d2c7ec69c56dbcf0b78e04554a966c
anton <antnzhlnkn@gmail.com>
2019-10-13 22:37:01 +0300

    Updates

---
+++ precache-manifest.40ae419874a0e9340ed92c56fad0c903.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bceb7bd294559802ef2c868475b66c",
+    "url": "/newtodo/index.html"
+  },
+  {
+    "revision": "0d6659bb97e4740d0890",
+    "url": "/newtodo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6eecb3e64555938f30c3",
+    "url": "/newtodo/static/js/2.4a0b62c4.chunk.js"
+  },
+  {
+    "revision": "0d6659bb97e4740d0890",
+    "url": "/newtodo/static/js/main.3f316b38.chunk.js"
+  },
+  {
+    "revision": "656f1ce1c0b58aff137a",
+    "url": "/newtodo/static/js/runtime-main.8ee34f10.js"
+  }
+]);
